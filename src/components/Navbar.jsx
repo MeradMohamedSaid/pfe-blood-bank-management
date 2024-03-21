1d559a91cbf382caa6d511687e46bac62d9c6ad8
louaye (lou) لُؤَيْ <louayekazar@gmail.com>
2024-03-21 16:46:53 +0100

    :ant:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="container">
-      <div></div>
+      {/* Nav Bar */}
+      <div className="flex justify-center items-center w-fit py-4">
+        <img className="w-16" src={logo} alt="LifeFlow Logo" />
+        <h1 className="text-3xl font-bold">LifeFlow</h1>
+      </div>
+      {/* Links */}
+      <div>
+        <Link to=""></Link>
+        <Link to=""></Link>
+        <Link to=""></Link>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+
+import "./font.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.jsx";
 import Login from "./pages/Login.jsx";

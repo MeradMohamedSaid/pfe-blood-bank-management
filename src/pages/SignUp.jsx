911f3db0a89837d446dcad10be6f808e68e99256
louaye (lou) لُؤَيْ <louayekazar@gmail.com>
2024-03-27 03:43:21 +0100

    :ambulance:

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import {
   LockClosedIcon,
   EyeIcon,
   EyeSlashIcon,
+  ArrowLeftIcon,
 } from "@heroicons/react/24/outline";
 import { useTranslation } from "react-i18next";
 import loginDrop from "../assets/loginDrop.png";
@@ -62,17 +63,294 @@ const Login = () => {
             <p>4</p>
           </div>
         </div>
-        
-        {/* Continue Button */}
-        <button
+
+        <h1
           className={
             i18n.language === "ar"
-              ? "max-w-[26rem] bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-arabic"
-              : "max-w-[26rem] bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-latin"
+              ? "text-5xl mb-8 text-center  text-arabic"
+              : "text-5xl mb-8 text-center  text-latin"
           }
         >
-          <h1>Continue</h1>
-        </button>
+          Create Your Account
+        </h1>
+        {steps === 1 ? (
+          <div className="w-[26rem]">
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <EnvelopeIcon className="w-6" />
+              <input
+                class="outline-none w-full"
+                type="text"
+                placeholder="name@example.com"
+              />
+            </div>
+
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Confirm Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+          </div>
+        ) : steps === 2 ? (
+          <div className="w-[26rem]">
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <EnvelopeIcon className="w-6" />
+              <input
+                class="outline-none w-full"
+                type="text"
+                placeholder="name@example.com"
+              />
+            </div>
+
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Confirm Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+          </div>
+        ) : steps === 3 ? (
+          <div className="w-[26rem]">
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <EnvelopeIcon className="w-6" />
+              <input
+                class="outline-none w-full"
+                type="text"
+                placeholder="name@example.com"
+              />
+            </div>
+
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Confirm Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="w-[26rem]">
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <EnvelopeIcon className="w-6" />
+              <input
+                class="outline-none w-full"
+                type="text"
+                placeholder="name@example.com"
+              />
+            </div>
+
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="bg-white p-4 rounded-xl flex justify-between items-center gap-2 w-full border border-red text-red mb-4">
+              <LockClosedIcon className="w-6" />
+              <input
+                placeholder="Confirm Password"
+                type={!pwVisible ? "text" : "Password"}
+                class="outline-none w-full"
+              />
+              {pwVisible ? (
+                <EyeIcon
+                  onClick={() => {
+                    setPwVisible(false);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              ) : (
+                <EyeSlashIcon
+                  onClick={() => {
+                    setPwVisible(true);
+                  }}
+                  className="w-6 cursor-pointer"
+                />
+              )}
+            </div>
+          </div>
+        )}
+        {/* Continue Button */}
+        {steps === 1 ? (
+          <button
+            onClick={() => {
+              if (steps < 5) {
+                setSteps(steps + 1);
+              }
+            }}
+            className={
+              i18n.language === "ar"
+                ? "max-w-[26rem] bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-arabic"
+                : "max-w-[26rem] bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-latin"
+            }
+          >
+            <h1>Continue</h1>
+          </button>
+        ) : (
+          <div className="min-w-[26rem] grid grid-cols-6 gap-2 grid-rows-1">
+            <button
+              onClick={() => {
+                if (steps > 1) {
+                  setSteps(steps - 1);
+                }
+              }}
+              className="bg-transparent text-red flex justify-center items-center border border-red rounded-lg hover:bg-red hover:text-white duration-300"
+            >
+              <ArrowLeftIcon className="w-6" />
+            </button>
+            <button
+              onClick={() => {
+                if (steps < 4) {
+                  setSteps(steps + 1);
+                  console.log(steps);
+                }
+              }}
+              className={
+                i18n.language === "ar"
+                  ? "col-span-5 bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-arabic"
+                  : "col-span-5 bg-red text-center text-white w-full py-2 rounded-xl hover:bg-opacity-70 duration-300 cursor-pointer text-latin"
+              }
+            >
+              <h1>Continue</h1>
+            </button>
+          </div>
+        )}
       </div>
     </AppLayout>
   );
